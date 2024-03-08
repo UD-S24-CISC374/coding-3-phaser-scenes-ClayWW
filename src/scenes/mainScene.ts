139ada55be0e9f1eafbc4ea09151f93562904e2e
Clay <clayw@udel.edu>
2024-03-07 20:58:34 -0500

    Uploaded sprites

--- src/scenes/mainScene.ts
+++ src/scenes/mainScene.ts
@@ -1,18 +1,11 @@
 import Phaser from "phaser";
-import PhaserLogo from "../objects/phaserLogo";
-import FpsText from "../objects/fpsText";
 
 export default class MainScene extends Phaser.Scene {
-    fpsText: FpsText;
-
     constructor() {
         super({ key: "MainScene" });
     }
 
     create() {
-        new PhaserLogo(this, this.cameras.main.width / 2, 0);
-        this.fpsText = new FpsText(this);
-
         const message = `Phaser v${Phaser.VERSION}`;
         this.add
             .text(this.cameras.main.width - 15, 15, message, {
@@ -22,7 +15,5 @@ export default class MainScene extends Phaser.Scene {
             .setOrigin(1, 0);
     }
 
-    update() {
-        this.fpsText.update();
-    }
+    update() {}
 }
